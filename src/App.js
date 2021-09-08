a4383b233b55167d1622521ec17400f0bef4ff69
Yash Jangir <yashjangirrr@gmail.com>
2021-09-08 23:38:55 +0530

    IssuesFixed

--- src/App.js
+++ src/App.js
@@ -1,60 +1,63 @@
-import './App.css';
-import Navbar from './Components/Navbar';
-import TextForm from './Components/TextForm';
-import React, { useState } from 'react';
-import Alert from './Components/Alert';
-import About from './Components/About';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import TextForm from "./Components/TextForm";
+import React, { useState } from "react";
+import Alert from "./Components/Alert";
+import About from "./Components/About";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState('light');
-  const [alert, setAlert] = useState(null)
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
 
-  const showAlert = (message , type)=>{
-      setAlert({
-        msg: message,
-        type: type
-      })
-      setTimeout(() => {
-        setAlert(null);
-      }, 2000);
-  }
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
 
-  const toggleMode = ()=>{
-    if(mode === 'light'){
-      setMode('dark');
-      document.body.style.backgroundColor = '#042743';
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
       showAlert("Dark mode has been enabled", "success");
-    }
-    else{
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
       showAlert("Light mode has been enabled", "success");
     }
-  }
+  };
 
   return (
-  <>
-  <Router>  
-  <Navbar title="Text Utils" aboutText="About Us " mode={mode} toggleMode={toggleMode}/>
-  <Alert alert={alert}/>
-  <div className="container my-4">
-  <Switch>
-          <Route exact path="/about">
-            <About mode={mode}/>
-          </Route>
-          <Route exact path="/">
-            <TextForm heading="Enter the Text To Analyze" showAlert={showAlert} mode={mode}/>
-          </Route>
-        </Switch>
-  </div>
-  </Router>
-  </>
+    <>
+      <Router>
+        <Navbar
+          title="TextliZer"
+          aboutText="About Us "
+          mode={mode}
+          toggleMode={toggleMode}
+        />
+        <Alert alert={alert} />
+        <div className="container my-4">
+          <Switch>
+            <Route exact path="/about">
+              <About mode={mode} />
+            </Route>
+            <Route exact path="/">
+              <TextForm
+                heading="TextliZer : Enter the Text To Analyze"
+                showAlert={showAlert}
+                mode={mode}
+              />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
 
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,63 +1,102 @@
-import React, { useState } from 'react'
-
+import React, { useState } from "react";
 
 export default function TextForm(props) {
-    const [text, setText] = useState("")
-    const upClick = ()=>{
-        let newText= text.toUpperCase();
-        setText(newText)
-        props.showAlert("Text Changed into UpperCase" , "success")
-    }
-    const loClick = ()=>{
-        let newText= text.toLowerCase();
-        setText(newText)
-        props.showAlert("Text Changed into LowerCase" , "success")
-    }
-    const reClick = ()=>{
-        let newText= text.split("").reverse().join("");
-        setText(newText)
-        props.showAlert("Text Reversed" , "success")
-    }
-    const coClick = ()=>{
-        navigator.clipboard.writeText(text); 
-        props.showAlert("Copied to Clipboard!" , "success")
-    }
-    const clClick = ()=>{
-        let newText= "";
-        setText(newText)
-        props.showAlert("Text Cleared!" , "success")
-    }
-    const rmClick = ()=>{
-        let newText = text.split(/[ ]+/);
-        setText(newText.join(" "));
-        props.showAlert("Extra Spaces Removed!" , "success")
-    }
+  const [text, setText] = useState("");
+  const upClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Text Changed into UpperCase", "success");
+  };
+  const loClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert("Text Changed into LowerCase", "success");
+  };
+  const reClick = () => {
+    let newText = text.split("").reverse().join("");
+    setText(newText);
+    props.showAlert("Text Reversed", "success");
+  };
+  const coClick = () => {
+    navigator.clipboard.writeText(text);
+    props.showAlert("Copied to Clipboard!", "success");
+  };
+  const clClick = () => {
+    let newText = "";
+    setText(newText);
+    props.showAlert("Text Cleared!", "success");
+  };
+  const rmClick = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+    props.showAlert("Extra Spaces Removed!", "success");
+  };
 
-    const onChangeText = (event)=>{
-        setText(event.target.value)
-    }
-    return (
-        <>
-        <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}>
-            <h2>{props.heading}</h2>
-            <div className="mb-3">
-                <textarea className="form-control" id="myText" value={text} onChange={onChangeText}  style={{backgroundColor: props.mode==='dark'?'#13466e':'white', color: props.mode==='dark'?'white':'#042743'}} rows="8"></textarea>
-            </div>
-            <button className="btn btn-primary mx-2 my-2" onClick={upClick}>Covert to UpperCase</button>
-            <button className="btn btn-primary mx-2 my-2" onClick={loClick}>Covert to LowerCase</button>
-            <button className="btn btn-primary mx-2 my-2" onClick={reClick}>Reverse the Text</button>
-            <button className="btn btn-primary mx-2 my-2" onClick={rmClick}>Remove Extra Spaces</button>
-            <button className="btn btn-primary mx-2 my-2" onClick={coClick}>Copy Text</button>
-            <button className="btn btn-primary mx-2 my-2" onClick={clClick}>Clear Text</button>
-            
+  const onChangeText = (event) => {
+    setText(event.target.value);
+  };
+  return (
+    <>
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+      >
+        <h2>{props.heading}</h2>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            id="myText"
+            value={text}
+            onChange={onChangeText}
+            style={{
+              backgroundColor: props.mode === "dark" ? "#13466e" : "white",
+              color: props.mode === "dark" ? "white" : "#042743",
+            }}
+            rows="8"
+          ></textarea>
         </div>
-        <div className="Container my-5" style={{color: props.mode==='dark'?'white':'#042743'}}>
-            <h4>Summery</h4>
-            <p>{text.split(/\s/).filter((words)=>{return words.length!==0}).length} Words and {text.length} Characters</p> 
-            <p>{ 0.008*text.split(" ").filter((words)=>{return words.length!==0}).length} Minutes to Read (Average)</p>
-            <h4>Preview</h4>
-            <p>{text}</p>
-        </div>
-        </>
-    )
+        <button className="btn btn-primary mx-2 my-2" onClick={upClick}>
+          Convert to UpperCase
+        </button>
+        <button className="btn btn-primary mx-2 my-2" onClick={loClick}>
+          Convert to LowerCase
+        </button>
+        <button className="btn btn-primary mx-2 my-2" onClick={reClick}>
+          Reverse the Text
+        </button>
+        <button className="btn btn-primary mx-2 my-2" onClick={rmClick}>
+          Remove Extra Spaces
+        </button>
+        <button className="btn btn-primary mx-2 my-2" onClick={coClick}>
+          Copy Text
+        </button>
+        <button className="btn btn-primary mx-2 my-2" onClick={clClick}>
+          Clear Text
+        </button>
+      </div>
+      <div
+        className="Container my-5"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+      >
+        <h4>Summery</h4>
+        <p>
+          {
+            text.split(/\s/).filter((words) => {
+              return words.length !== 0;
+            }).length
+          }{" "}
+          Words and {text.length} Characters
+        </p>
+        <p>
+          {0.008 *
+            text.split(" ").filter((words) => {
+              return words.length !== 0;
+            }).length}{" "}
+          Minutes to Read (Average)
+        </p>
+        <h4>Preview</h4>
+        <p>{text}</p>
+      </div>
+    </>
+  );
 }
